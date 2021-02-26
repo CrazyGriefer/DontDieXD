de4336ac847175a734703d177e8e106aca81343d
CrazyGriefer <78539778+CrazyGriefer@users.noreply.github.com>
2021-02-26 15:09:14 +0100

    Delete src directory

--- src/me/crazygriefer/dontdiexd/GlobalVar.java
+++
@@ -1,9 +0,0 @@
-package me.crazygriefer.dontdiexd;
-
-import net.md_5.bungee.api.ChatColor;
-
-public class GlobalVar {
-	public static String Prefix = ChatColor.GOLD + "[" + ChatColor.AQUA + "DDXD" + ChatColor.GOLD + "] " + ChatColor.YELLOW;
-	public static Boolean DamageEnable = false;
-}
-
